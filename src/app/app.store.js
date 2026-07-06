7d6bd2b7e8021fe224eb85b44f957d5c450bef15
Ravi_Shankar_kr <ravi896959@gmail.com>
2026-07-06 19:55:48 +0530

    configure tailwind, alias and global styles

--- src/app/app.store.js
+++ src/app/app.store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/auth/authSlice";
-import productReducer from "../features/products/productSlice";
-import cartReducer from "../features/cart/cartSlice";
-import wishlistReducer from "../features/wishlist/wishlistSlice";
-import orderReducer from "../features/orders/orderSlice";
-import profileReducer from "../features/profile/profileSlice";
-import themeReducer from "../features/theme/themeSlice";
+import authReducer from "@/features/auth/authSlice";
+import productReducer from "@/features/products/productSlice";
+import cartReducer from "@/features/cart/cartSlice";
+import wishlistReducer from "@/features/wishlist/wishlistSlice";
+import orderReducer from "@/features/orders/orderSlice";
+import profileReducer from "@/features/profile/profileSlice";
+import themeReducer from "@/features/theme/themeSlice";
 
 export const store = configureStore({
   reducer: {
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import App from "./app/App";
-import { store } from "./app/app.store";
-import "./styles/index.css";
+import App from "@/app/App";
+import { store } from "@/app/app.store";
+import "@/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
